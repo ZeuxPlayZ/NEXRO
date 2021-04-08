275de81f52855cff17d4cbe08031b8339ebea2e5
ZeuxPlayZ <Shashwatsingh0402@gmail.com>
2021-04-08 10:58:03 +0530

    NEXRO

---
+++ commands/skip.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "skip",
+    aliases: ["S"],
+    inVoiceChannel: true,
+    run: async (client, message, args) => {
+
+
+        
+        const queue = client.distube.getQueue(message)
+        if (!queue) return message.channel.send(`${client.emotes.error} | There is nothing in the queue right now!`)
+        try {
+            client.distube.skip(message)
+            message.channel.send(`${client.emotes.success} | Skipped! Now playing:\n${queue.songs[0].name}`)
+        } catch (e) {
+            message.channel.send(`${client.emotes.error} | ${e}`)
+        }
+    }
+}
